bd46ade56ee639b87de74eb99a8f6eb5d7bda05c
adnan-akhlas <mail.adnanakhlas@gmail.com>
2026-02-24 23:44:56 +0600

    chore: react router initialized

---
+++ src/router/RouteHandler.tsx
@@ -0,0 +1,5 @@
+import { Navigate } from "react-router";
+
+export default function RouterHandler() {
+  return <Navigate to={"/auth/login"} />;
+}
---
+++ src/router/index.ts
@@ -0,0 +1,32 @@
+import AuthLayout from "@/components/layouts/AuthLayout";
+import LoginPage from "@/pages/LoginPage";
+import RegisterPage from "@/pages/RegisterPage";
+import { createBrowserRouter } from "react-router";
+import RouterHandler from "@/router/RouteHandler";
+import DashboardLayout from "@/components/layouts/DashboardLayout";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RouterHandler,
+  },
+  {
+    path: "/auth",
+    Component: AuthLayout,
+    children: [
+      {
+        path: "login",
+        Component: LoginPage,
+      },
+      {
+        path: "register",
+        Component: RegisterPage,
+      },
+    ],
+  },
+  {
+    path: "/dashboard",
+    Component: DashboardLayout,
+    children: [],
+  },
+]);
