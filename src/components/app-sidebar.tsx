f8e0c06dc48945423e9a3c63d6048bb0b4c32aaa
adnan-akhlas <mail.adnanakhlas@gmail.com>
2026-02-25 01:00:04 +0600

    style(ui/dashboard): create dashboard layout

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,77 @@
+import {
+  IconChartBar,
+  IconDashboard,
+  IconFolder,
+  IconInnerShadowTop,
+  IconListDetails,
+  IconUsers,
+} from "@tabler/icons-react";
+import * as React from "react";
+
+import { NavMain } from "@/components/nav-main";
+import { NavUser } from "@/components/nav-user";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+const navMain = [
+  {
+    title: "Dashboard",
+    url: "#",
+    icon: IconDashboard,
+  },
+  {
+    title: "Lifecycle",
+    url: "#",
+    icon: IconListDetails,
+  },
+  {
+    title: "Analytics",
+    url: "#",
+    icon: IconChartBar,
+  },
+  {
+    title: "Projects",
+    url: "#",
+    icon: IconFolder,
+  },
+  {
+    title: "Team",
+    url: "#",
+    icon: IconUsers,
+  },
+];
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="offcanvas" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:!p-1.5"
+            >
+              <a href="#">
+                <IconInnerShadowTop className="!size-5" />
+                <span className="text-base font-semibold">eBook Portal</span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser />
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
