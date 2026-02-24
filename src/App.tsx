a162ac352a409168c879242f7bf59618a2c7ebbb
adnan-akhlas <mail.adnanakhlas@gmail.com>
2026-02-24 22:43:30 +0600

    chore: tailwind & shadcn initialized

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-4xl text-violet-500 underline">
+        Welcome to ebook portal dashboard.
+      </h1>
+      <Button>Click Me</Button>
+    </div>
+  );
+}
